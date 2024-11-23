37a83d660f89c40d4152622bb261f81efbbf3c4f
romashkoyyt <ladnessiss@gmail.com>
2024-11-24 00:16:06 +0300

    Add files via upload

---
+++ src/main/java/org/romashkoyyt/TokenType.java
@@ -0,0 +1,70 @@
+package org.romashkoyyt;
+
+public enum TokenType {
+    NUMBER,
+    HEX,
+    WORD,
+    STRING,
+    TRUE,
+    FALSE,
+
+    PRINT,
+    READ,
+    WRITE,
+    TONUM,
+    TOSTR,
+    FORMAT,
+    RAND,
+
+    PLUS,
+    MINUS,
+    SLASH,
+    STAR,
+
+    EQ,
+    EQEQ,
+    GT,
+    LT,
+    GTEQ,
+    LTEQ,
+    NEQ,
+    NOT,
+
+    LPAREN,
+    RPAREN,
+
+    VAR,
+    SEMICOLON,
+
+    IF,
+    ELSE,
+    THEN,
+    END,
+
+    WHILE,
+
+    FOR,
+    FOREACH,
+    COM,
+    AND,
+    ANDAND,
+    OR,
+    TO,
+    DOT,
+
+    POWER,
+
+    FUNC,
+    RETURN,
+    INCLUDE,
+
+    CLASS,
+    CLOSED,
+    STATIC,
+
+    OBJ,
+    TAB,
+    COMMENT,
+
+    EOF
+}
